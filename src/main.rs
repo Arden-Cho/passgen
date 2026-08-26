be334f7c9af302235a0c00eb4ad6a393e6840c86
Arden <hi@easoncho.com>
2026-08-26 07:18:26 +0000

    use less heap allocs by using extend and clear

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ struct Args {
     /// Include special characters [!@#$%^&*]
     #[arg(short, long)]
     special: bool,
-    
+
     /// The length of the password
     #[arg(short = 'L', long, default_value_t = 16)]
     length: u8,
@@ -59,28 +59,27 @@ fn generate_password(args: &Args) -> String {
         }
         s.into_bytes()
     };
-    let mut password: String;
+    let mut password: String = String::with_capacity(args.length as usize);
     loop {
         let mut args = args.clone();
-        password = (0..args.length)
-            .map(|_| {
-                let idx = random_range_usize(0..chars.len());
-                let c = chars[idx] as char;
-                if c.is_ascii_uppercase() {
-                    args.upper = false;
-                } else if c.is_ascii_lowercase() {
-                    args.lower = false;
-                } else if c.is_ascii_digit() {
-                    args.numbers = false;
-                } else {
-                    args.special = false;
-                }
-                c
-            })
-            .collect();
+        password.extend((0..args.length).map(|_| {
+            let idx = random_range_usize(0..chars.len());
+            let c = chars[idx] as char;
+            if c.is_ascii_uppercase() {
+                args.upper = false;
+            } else if c.is_ascii_lowercase() {
+                args.lower = false;
+            } else if c.is_ascii_digit() {
+                args.numbers = false;
+            } else {
+                args.special = false;
+            }
+            c
+        }));
         if !(args.upper || args.lower || args.numbers || args.special) {
             break;
         }
+        password.clear();
     }
     password
 }
