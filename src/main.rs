f6a618caafb269dd6afd256a327714bd64618e86
Arden <hi@easoncho.com>
2026-08-27 03:29:48 +0000

    format

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,19 @@ mod utils;
 
 use clap::Parser;
 
-use crate::{args::Args, generators::generate_password, utils::{log}};
+use crate::{args::Args, generators::generate_password, utils::log};
 
 fn main() {
     let args = Args::parse().normalize();
     if args.entropy {
         let entropy = args.get_entropy();
-        log(&"entropy", &format!("{0:.2} bits - this could indicate a {1} password", entropy.0, entropy.1));
+        log(
+            &"entropy",
+            &format!(
+                "{0:.2} bits - this could indicate a {1} password",
+                entropy.0, entropy.1
+            ),
+        );
     }
     println!("{}", generate_password(&args));
 }
